a0cc78c1f098396da3b59377e6b1695ac7e7e471
Julio <julio.olvr@gmail.com>
2020-07-14 23:54:17 -0300

    Tweak admin dashboards

--- app/dashboards/game_dashboard.rb
+++ app/dashboards/game_dashboard.rb
@@ -24,21 +24,20 @@ class GameDashboard < Administrate::BaseDashboard
   # By default, it's limited to four items to reduce clutter on index pages.
   # Feel free to add, remove, or rearrange items.
   COLLECTION_ATTRIBUTES = %i[
-    platforms
-    reviews
     id
     name
+    platforms
   ].freeze
 
   # SHOW_PAGE_ATTRIBUTES
   # an array of attributes that will be displayed on the model's show page.
   SHOW_PAGE_ATTRIBUTES = %i[
-    platforms
-    reviews
     id
     name
     created_at
     updated_at
+    platforms
+    reviews
   ].freeze
 
   # FORM_ATTRIBUTES
--- app/dashboards/platform_dashboard.rb
+++ app/dashboards/platform_dashboard.rb
@@ -23,27 +23,26 @@ class PlatformDashboard < Administrate::BaseDashboard
   # By default, it's limited to four items to reduce clutter on index pages.
   # Feel free to add, remove, or rearrange items.
   COLLECTION_ATTRIBUTES = %i[
-    games
-    id
-    name
-    created_at
-  ].freeze
-
-  # SHOW_PAGE_ATTRIBUTES
-  # an array of attributes that will be displayed on the model's show page.
-  SHOW_PAGE_ATTRIBUTES = %i[
-    games
     id
     name
     created_at
     updated_at
   ].freeze
 
+  # SHOW_PAGE_ATTRIBUTES
+  # an array of attributes that will be displayed on the model's show page.
+  SHOW_PAGE_ATTRIBUTES = %i[
+    id
+    name
+    created_at
+    updated_at
+    games
+  ].freeze
+
   # FORM_ATTRIBUTES
   # an array of attributes that will be displayed
   # on the model's form (`new` and `edit`) pages.
   FORM_ATTRIBUTES = %i[
-    games
     name
   ].freeze
 
@@ -62,7 +61,7 @@ class PlatformDashboard < Administrate::BaseDashboard
   # Overwrite this method to customize how platforms are displayed
   # across all pages of the admin dashboard.
   #
-  # def display_resource(platform)
-  #   "Platform ##{platform.id}"
-  # end
+  def display_resource(platform)
+    platform.name
+  end
 end
