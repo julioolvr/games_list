b841f8cd76b3808e3cb6a502e70fba7b9c3d329e
Julio <julio.olvr@gmail.com>
2020-07-18 10:21:49 -0300

    Validate review score

--- app/models/review.rb
+++ app/models/review.rb
@@ -5,5 +5,7 @@ class Review < ApplicationRecord
   belongs_to :game
 
   validates :body, presence: true
-  validates :score, presence: true
+  validates :score,
+            presence: true,
+            numericality: { only_integer: true, greater_than_or_equal_to: -1, less_than_or_equal_to: 2 }
 end
